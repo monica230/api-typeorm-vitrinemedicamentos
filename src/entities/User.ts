1243b25eade9da236af610a749a3174163a66f7b
monica230 <monicamaltacouto@gmail.com>
2025-01-26 22:06:33 -0300

    feat: create entity user

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,3 +1,27 @@
-class User {
-    
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity('users')
+export class User {
+
+@PrimaryGeneratedColumn('increment')  
+id:number;  
+
+@Column('varchar',{ length: 255, nullable: false})
+nome:string;
+
+@Column('varchar',{ length: 255, nullable: false,unique: true})
+email:string;
+
+@Column('varchar',{ length: 255, nullable: false})
+senha:string;
+
 }
+
+/*id (chave primária, auto-incremento)
+
+nome (string)
+
+email (string, único)
+
+senha (string, hash)*/
