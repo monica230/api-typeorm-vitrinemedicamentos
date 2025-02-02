120985ca34e23942b0f91de0a49fbed002f58e48
monica230 <monicamaltacouto@gmail.com>
2025-02-02 15:22:21 -0300

    correcao

---
+++ src/migration/1738461913896-createTableMedicamentos.ts
@@ -0,0 +1,55 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateTableMedicamentos1738461913896
+    implements MigrationInterface
+{
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'medicamentos',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'int',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment',
+                    },
+                    {
+                        name: 'name',
+                        type: 'int',
+                        length: '255',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'descricao',
+                        type:'text',
+                        isNullable:true,                    },
+                        {
+                            name:'quantidade',
+                        type: 'int',
+                        isGenerated:false,
+                        },
+                        {
+                            name:'userId',
+                            type:'int',
+                            isNullable:false,
+                        },
+            ],
+            foreignKeys:[
+                {
+                    columnNames:['userId'],
+                    referencedColumnNames:['id'],
+                    referencedTableName:'users',
+                    onDelete:'CASCADE',
+                    onUpdate:'CASCADE',
+                },
+            ],
+        }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('medicamentos');
+    }
+}
