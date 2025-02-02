9c8c8c77af4a906c0728a582a111720cebf61ae8
monica230 <monicamaltacouto@gmail.com>
2025-02-01 22:08:49 -0300

    feat: adicionei JWT autentiquei e atualizar as rotas de usuário

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,33 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { AppDataSource } from './database/data-source';
+import userRoutes from './routes/user.routes';
 
 const app = express();
+
 dotenv.config();
 
-const porta = process.env.PORTA ||3000;
+const porta =Number (process.env.PORTA) ||3000;
 
-app.listen(3000, () => {
-    console.log(`Servidor rodando na porta ${porta}`);
-});
+app.use(express.json());
+
+
+
+AppDataSource.initialize()
+.then(()=>{
+
+console.log('Conexão com o banco de dados realizada com sucesso!');
+
+app.use("/use", userRoutes);
+
+    app.listen(porta, () => {
+        console.log(`Servidor rodando na porta ${porta}`);
+    });
+})
+.catch((error) => (
+    console.log(
+        'Error ao conectar no banco de dados:',error)
+    ))
+
+
+    
